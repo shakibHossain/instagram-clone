055a095110da07b5dfa684bbc2faca5c0e4db447
shakibHossain <shakib.hossain.sh@gmail.com>
2020-07-10 05:00:20 -0600

    add app header with ig logo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import './App.css';
-import Post from './Post';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App__header">
-        
-      </header>
-      <h1>Hello World!</h1>
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+      </div>
 
-      <Post/>
+      <h1>Hello World!</h1>
     </div>
   );
 }
