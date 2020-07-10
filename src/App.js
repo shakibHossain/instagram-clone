fff8278ba06bf07d3f7f569e0ecbfc28ee6329e0
shakibHossain <shakib.hossain.sh@gmail.com>
2020-07-10 05:20:02 -0600

    add post component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Post from "./Post";
 
 function App() {
   return (
@@ -13,6 +14,10 @@ function App() {
       </div>
 
       <h1>Hello World!</h1>
+
+      <Post />
+      {/* Posts */}
+      {/* Posts */}
     </div>
   );
 }
--- src/Post.js
+++ src/Post.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import './Post.css';
+import React from "react";
+import "./Post.css";
 
 function Post() {
-    return (
-        <div className="post">
-            <h3>Username</h3>
-            {/* header -> avatar + username */}
+  return (
+    <div className="post">
+      <h3>Username</h3>
+      {/* header -> avatar + username */}
 
-            <img class = "post__image" src=""></img>
-            {/* image */}
+      <img
+        class="post__image"
+        src="https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
+        alt=""
+      />
+      {/* image */}
 
-            <h4>Username: caption</h4>
-            {/* username + caption*/}
-            
-        </div>
-    )
+      <h4 className="post__text">
+        <strong>shakib</strong> WOW day three of live sessions
+      </h4>
+      {/* username + caption*/}
+    </div>
+  );
 }
 
-export default Post
+export default Post;
