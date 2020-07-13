277da17307f6057a8837bd6846a4b730a1994498
shakibHossain <shakib.hossain.sh@gmail.com>
2020-07-12 18:38:38 -0600

    add comment field to posts

--- src/Post.js
+++ src/Post.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Post.css";
 import { Avatar } from "@material-ui/core";
+import { db } from "./firebase";
 
-function Post({ username, caption, imageUrl }) {
+function Post({ postId, username, caption, imageUrl }) {
+  const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState("");
+
+  useEffect(() => {
+    let unsubscribe;
+    if (postId) {
+      unsubscribe = db
+        .collection("posts")
+        .doc(postId)
+        .collection("comments")
+        .onSnapshot((snapshot) => {
+          setComments(snapshot.docs.map((doc) => doc.data()));
+        });
+    }
+
+    return () => {
+      unsubscribe();
+    };
+  }, [postId]);
+
+  const postComment = (event) => {};
+
   return (
     <div className="post">
       <div className="post__header">
@@ -22,6 +45,23 @@ function Post({ username, caption, imageUrl }) {
         <strong>{username}</strong> {caption}
       </h4>
       {/* username + caption*/}
+      <form className="post__commentBox">
+        <input
+          className="post__input"
+          type="text"
+          placeholder="Add a comment..."
+          value={comment}
+          onChange={(e) => setComments(e.target.value)}
+        />
+        <button
+          className="post__button"
+          disabled={!comment}
+          type="submit"
+          onClick={postComment}
+        >
+          Post
+        </button>
+      </form>
     </div>
   );
 }
