afb18d91087978ce0f7581a4473cd197d5b9b36b
shakibHossain <shakib.hossain.sh@gmail.com>
2020-07-12 19:02:59 -0600

    add comment, view comments functionalities added

--- src/Post.js
+++ src/Post.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from "react";
 import "./Post.css";
 import { Avatar } from "@material-ui/core";
 import { db } from "./firebase";
+import firebase from "firebase";
 
-function Post({ postId, username, caption, imageUrl }) {
+function Post({ user, postId, username, caption, imageUrl }) {
   const [comments, setComments] = useState([]);
   const [comment, setComment] = useState("");
 
@@ -14,6 +15,7 @@ function Post({ postId, username, caption, imageUrl }) {
         .collection("posts")
         .doc(postId)
         .collection("comments")
+        .orderBy("timestamp", "desc")
         .onSnapshot((snapshot) => {
           setComments(snapshot.docs.map((doc) => doc.data()));
         });
@@ -24,7 +26,16 @@ function Post({ postId, username, caption, imageUrl }) {
     };
   }, [postId]);
 
-  const postComment = (event) => {};
+  const postComment = (event) => {
+    event.preventDefault();
+
+    db.collection("posts").doc(postId).collection("comments").add({
+      text: comment,
+      username: user.displayName,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    setComment("");
+  };
 
   return (
     <div className="post">
@@ -44,6 +55,14 @@ function Post({ postId, username, caption, imageUrl }) {
       <h4 className="post__text">
         <strong>{username}</strong> {caption}
       </h4>
+      <div className="post__comments">
+        {comments.map((comment) => (
+          <p>
+            <strong>{comment.username} </strong>
+            {comment.text}
+          </p>
+        ))}
+      </div>
       {/* username + caption*/}
       <form className="post__commentBox">
         <input
@@ -51,7 +70,7 @@ function Post({ postId, username, caption, imageUrl }) {
           type="text"
           placeholder="Add a comment..."
           value={comment}
-          onChange={(e) => setComments(e.target.value)}
+          onChange={(e) => setComment(e.target.value)}
         />
         <button
           className="post__button"
